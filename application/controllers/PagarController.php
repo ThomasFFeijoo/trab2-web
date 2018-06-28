ac9cb7fed13e10d7ccf5b3edc4d9d497aec08345
lucasmpalma <lucaspalma.m@gmail.com>
2018-06-28 17:46:40 -0300

    debug

--- application/controllers/PagarController.php
+++ application/controllers/PagarController.php
@@ -6,7 +6,23 @@ class PagarController extends AbstractBaseController
     public function indexAction()
     {
 
-    	var_dump($this->getParam("id"));
-    	exit();
+    	$id = $this->getParam("id");
+    	
+    	$ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL,"http://35.198.42.142:3000/payCart/".$id);
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+// in real life you should use something like:
+// curl_setopt($ch, CURLOPT_POSTFIELDS, 
+//          http_build_query(array('postvar1' => 'value1')));
+
+// receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+var_dump($server_output);
+curl_close ($ch);
+exit();
     }
 }
